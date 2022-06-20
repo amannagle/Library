6ac2974533d374d1c0fa7acaebf35903320d275c
aman <aman.nagle1999@gmail.com>
2022-06-20 13:50:04 +0530

    added delete and edit functionality

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ submit.addEventListener('click',function()
     const title=document.querySelector('#title').value;
     const author=document.querySelector('#author').value;
     const pages=document.querySelector('#pages').value;
-    const read=document.querySelector('#hasread').value;
+    const read=document.querySelector('#hasread').checked;
     const book= new Book(title,author,pages,read);
     Books.push(book);
     document.querySelector('.popup').classList.remove('active');
@@ -41,6 +41,7 @@ function displayBooks()
         const div = document.createElement('div');
         bookcontainer.appendChild(div);
         div.classList.add('books');
+        div.setAttribute('index',Books.indexOf(book));
         for (const [key, value] of Object.entries(book)) {
             if(key=="display")
             continue;
@@ -52,11 +53,12 @@ function displayBooks()
                 const span=document.createElement('span');
                 span.textContent=`Read?`
                 const input=document.createElement('input');
+                input.checked=value;
                 input.setAttribute('type','checkbox');
-                input.value=value;
                 child_div.append(span);
                 child_div.append(input);
                 book.display="yes";
+                input.disabled=true;
             }
             else
             {
@@ -71,6 +73,7 @@ function displayBooks()
             
 
          }
+ 
         //  const icons=document.querySelector('.icons');
          const icons=document.createElement('div');
          const icon1=document.createElement('icon');
@@ -83,6 +86,39 @@ function displayBooks()
          div.appendChild(icons);
          icons.appendChild(icon1);
          icons.appendChild(icon2);
+         const functions = document.querySelectorAll('icon');
+         console.log(functions);
+         functions.forEach((x)=>{
+         x.addEventListener('click',deleteOrEdit);
+         })
     }
 }
 
+
+
+
+function deleteOrEdit(e)
+{
+    console.log(e.target.classList);
+    if(e.target.classList.value == 'fa-solid fa-trash')
+    {
+        const books_div = e.target.parentNode.parentNode;
+        const index=books_div.getAttribute('index');
+        Books.splice(index,1);
+        books_div.remove();
+        const books = document.querySelectorAll('.books');
+        books.forEach((book)=>{
+            if(book.getAttribute('index')>index)
+            book.setAttribute('index',book.getAttribute('index')-1);
+        })        
+    }
+
+    if(e.target.classList.value == 'fa-solid fa-pencil')
+    {
+        const books_div = e.target.parentNode.parentNode;
+        const input_div = books_div.childNodes[3];
+        const checkbox = input_div.lastChild;
+        checkbox.disabled=false;
+        console.log(checkbox);
+    }
+}
